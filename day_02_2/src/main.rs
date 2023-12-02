6f138ca7ec6cd0f47a259a3cfee4f1ee04ca5328
Josh Hayden <6366462+ShadowTigger333@users.noreply.github.com>
2023-12-02 14:56:42 -0800

    Advent of Code 2023, Day 2 Part 2 Solution

---
+++ day_02_2/src/main.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use nom::IResult;
+use nom::bytes::complete::tag;
+use nom::character::complete::{i32, alpha1};
+use nom::sequence::{delimited, tuple};
+
+fn main() {
+    let mut sum = 0;
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            let mut max_red = 0;
+            let mut max_green = 0;
+            let mut max_blue = 0;
+
+            if let Ok(line) = line {
+                let (input, _id) = find_id(&line).expect("Id");
+                let new_input = input.replace(',', ";").replace(" ", "");
+                new_input.split(";").for_each(|item| {
+                    let (_, (ammount, key)) = get_item(item).expect("Item");
+                    match key {
+                        "green" => {if ammount > max_green {
+                            max_green = ammount;
+                        }},
+                        "blue"=> {if ammount > max_blue {
+                            max_blue = ammount;
+                        }},
+                        "red"=> {if ammount > max_red {
+                            max_red = ammount;
+                        }},
+                        _ => panic!("This is bad")
+                    }
+                });
+                let power = max_blue*max_green*max_red;
+                sum += power;
+                println!("ADDED: {}", power)
+            }
+        }
+    }
+    println!("The total sum is: {}", sum)
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+fn find_id(input: &str) -> IResult<&str, i32> {
+    delimited(tag("Game "), i32, tag(":"))(input)
+}
+fn get_item(input: &str) -> IResult<&str, (i32, &str)> {
+    tuple((i32, alpha1))(input)
+}
